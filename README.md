13c7cf7cc5b61b54ade43201466aba1d2deb4d17
mhavelant <mhavelant@brainsum.com>
2017-12-11 17:47:23 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# General Data Protection Regulation
+## What it does
+This module gives end user visibility to the data stored about himself/herself and aims to help site admins follows the guidelines and legislation set by the EU.
+
+## Current features
+* Allow logged in user to see all raw data stored about himself/herself (user entity)
+* Allow user to initiate “forget me” action from site admins
+* Checklist for site admin (recommend modules like cookie consent, check if there is privacy policy page etc)
+
+## Planned features
+* Make sure user can rectify all data about himself/herself
+* Allow user to remove the account (content is not removed)
+* More items and recommendations to checklist
+* Add Drush hooks to sanitize data when syncing databases
+* Make API for other contrib modules to announce user data stored
+
+## Also needed
+* Tests
+
+## More information about GDPR
+* https://www.eugdpr.org/
+* [GDPR compliance in core](https://www.drupal.org/project/drupal/issues/2848974)
+* https://www.itgovernance.co.uk/data-protection-dpa-and-eu-data-protection-regulation
+* https://www.csoonline.com/article/3202771/data-protection/general-data-protection-regulation-gdpr-requirements-deadlines-and-facts.html
---
+++ src/Controller/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\gdpr\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\UserInterface;
+
+/**
+ * Class UserController.
+ *
+ * @package Drupal\gdpr\Controller
+ */
+class UserController extends ControllerBase {
+
+  /**
+   * Access check for the route.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   The given user.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The result.
+   */
+  public function access(UserInterface $user) {
+    if ((int) $user->id() === (int) $this->currentUser()->id()) {
+      return AccessResult::allowed();
+    }
+
+    return AccessResult::forbidden();
+  }
+
+  /**
+   * Return the collected data for a user.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   The user.
+   *
+   * @return array
+   *   Render array.
+   */
+  public function collectedData(UserInterface $user) {
+    $rows = [];
+    /** @var \Drupal\Core\Field\FieldItemListInterface $field */
+    foreach ($user as $key => $field) {
+      // Don't show the hash.
+      if ('pass' === $key) {
+        $rows[] = [
+          'type' => $field->getName(),
+          'value' => '******',
+        ];
+        continue;
+      }
+
+      $fieldValue = $field->getString();
+
+      // @todo: Maybe display as N/A, or as empty?
+      if (empty($fieldValue)) {
+        continue;
+      }
+
+      // @todo: Maybe turn to human readable values?
+      $rows[] = [
+        'type' => $key,
+        'value' => $fieldValue,
+      ];
+    }
+
+    $table = [
+      '#type' => 'table',
+      '#caption' => $this->t('Stored u0ser data'),
+      '#header' => [
+        $this->t('Type'),
+        $this->t('Value'),
+      ],
+      '#rows' => $rows,
+    ];
+
+    return $table;
+  }
+
+}
