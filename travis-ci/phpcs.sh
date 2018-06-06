bdad76931862e75b29372b60e47cf72f697c6e84
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2018-06-06 12:14:18 +0000

    by yanniboi: Exclude certain sniffs for coder review.

--- travis-ci/phpcs.sh
+++ travis-ci/phpcs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$PHPCS_WARNINGS" != "warnings" ]; then
-  $DRUPAL_BUILD_ROOT/drupal/vendor/bin/phpcs $TRAVIS_BUILD_DIR -p --ignore=$TRAVIS_BUILD_DIR/README.md --standard=Drupal --colors
+  $DRUPAL_BUILD_ROOT/drupal/vendor/bin/phpcs $TRAVIS_BUILD_DIR -p --ignore=$TRAVIS_BUILD_DIR/README.md --standard=phpcs.xml --colors
 else
   $DRUPAL_BUILD_ROOT/drupal/vendor/bin/phpcs $TRAVIS_BUILD_DIR -p --ignore=$TRAVIS_BUILD_DIR/README.md --standard=DrupalPractice --colors
 fi
