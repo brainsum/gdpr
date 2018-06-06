ba02b2ba931a80ca97f6129bd47301dad6d8d2c8
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2018-06-06 12:19:24 +0000

    by yanniboi: Non 0 warning exit code.

--- travis-ci/phpcs.sh
+++ travis-ci/phpcs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 if [ "$PHPCS_WARNINGS" != "warnings" ]; then
   $DRUPAL_BUILD_ROOT/drupal/vendor/bin/phpcs $TRAVIS_BUILD_DIR -p --ignore=$TRAVIS_BUILD_DIR/README.md --standard=phpcs.xml --colors
 else
+  $DRUPAL_BUILD_ROOT/drupal/vendor/bin/phpcs --config-set ignore_warnings_on_exit 1
   $DRUPAL_BUILD_ROOT/drupal/vendor/bin/phpcs $TRAVIS_BUILD_DIR -p --ignore=$TRAVIS_BUILD_DIR/README.md --standard=DrupalPractice --colors
 fi
