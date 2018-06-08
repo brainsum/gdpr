fa39f3291d812e8e6b972426eb93d453f91b4b20
szato <tszanyi@brainsum.com>
2018-06-08 11:51:41 +0200

    Revert "Avoid error: Class 'Drupal\entity\Routing\AdminHtmlRouteProvider' not found."
    This reverts commit 67cbc3662b346f3cb05d5aa677a7701f7cd80467.
    
    We should depend on drupal/entity:^1.0.0-beta2 which contains 'Drupal\entity\Routing\AdminHtmlRouteProvider'.
    FYI: entity beta2 depends on drupal/core ~8.5

--- modules/gdpr_tasks/src/Entity/Task.php
+++ modules/gdpr_tasks/src/Entity/Task.php
@@ -30,7 +30,7 @@ use Drupal\user\UserInterface;
  *     },
  *     "access" = "Drupal\gdpr_tasks\TaskAccessControlHandler",
  *     "route_provider" = {
- *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *       "html" = "Drupal\entity\Routing\AdminHtmlRouteProvider",
  *     },
  *   },
  *   base_table = "gdpr_task",
