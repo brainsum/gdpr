e2388246402062d7a8fb91fbcc3c39a3d17d570d
mhavelant <mhavelant@brainsum.com>
2018-06-12 12:18:18 +0200

    GDPR Tasks settings form string fixes.

--- modules/gdpr_tasks/src/Form/RemovalSettingsForm.php
+++ modules/gdpr_tasks/src/Form/RemovalSettingsForm.php
@@ -36,7 +36,7 @@ class RemovalSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['directory'] = [
       '#type' => 'textfield',
-      '#title' => 'Export Directory',
+      '#title' => $this->t('Export Directory'),
       '#description' => $this->t('Specifies the path to the directory where Right to be Forgotten tasks are exported after being processed'),
       '#default_value' => $this->config(self::CONFIG_KEY)
         ->get(self::EXPORT_DIRECTORY),
@@ -50,9 +50,9 @@ class RemovalSettingsForm extends ConfigFormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
     if ($form_state->hasValue('directory')) {
-      $dir = $form_state->getValue('directory');
-      if (!empty($dir) && !file_prepare_directory($dir)) {
-        $form_state->setErrorByName('directory', $this->t('The directory does not exist.'));
+      $directory = $form_state->getValue('directory');
+      if (!empty($directory) && !\file_prepare_directory($directory)) {
+        $form_state->setErrorByName('directory', $this->t("The directory does not exist or it's not writable."));
       }
     }
   }
@@ -61,7 +61,6 @@ class RemovalSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
     $this->config(self::CONFIG_KEY)
       ->set(self::EXPORT_DIRECTORY, $form_state->getValue('directory'))
       ->save();
