25d5ee7023323408f3a80c0a7dd7858c5352f383
mhavelant <mhavelant@brainsum.com>
2018-07-19 15:41:59 +0200

    Minor update for the "Removal requests form".

--- modules/gdpr_tasks/src/Form/RemovalSettingsForm.php
+++ modules/gdpr_tasks/src/Form/RemovalSettingsForm.php
@@ -13,7 +13,6 @@ use Drupal\Core\Form\FormStateInterface;
 class RemovalSettingsForm extends ConfigFormBase {
 
   const CONFIG_KEY = 'gdpr_tasks.settings';
-
   const EXPORT_DIRECTORY = 'export_directory';
 
   /**
@@ -38,6 +37,7 @@ class RemovalSettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#title' => $this->t('Export Directory'),
       '#description' => $this->t('Specifies the path to the directory where Right to be Forgotten tasks are exported after being processed'),
+      '#required' => TRUE,
       '#default_value' => $this->config(self::CONFIG_KEY)
         ->get(self::EXPORT_DIRECTORY),
     ];
@@ -51,8 +51,13 @@ class RemovalSettingsForm extends ConfigFormBase {
     parent::validateForm($form, $form_state);
     if ($form_state->hasValue('directory')) {
       $directory = $form_state->getValue('directory');
-      if (!empty($directory) && !\file_prepare_directory($directory)) {
+      if (empty($directory)) {
+        $form_state->setErrorByName('directory', $this->t('The directory is required.'));
+        return;
+      }
+      if (!\file_prepare_directory($directory)) {
         $form_state->setErrorByName('directory', $this->t("The directory does not exist or it's not writable."));
+        return;
       }
     }
   }
